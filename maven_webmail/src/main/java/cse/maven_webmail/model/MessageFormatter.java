1c190863d53c5b5770f5ae8d60bf7128f8057545
EhitEpit <chungil987@naver.com>
2021-05-25 04:35:40 +0900

    메일 임시 삭제(MessageFormatter.java) - 완전화 유지보수 MessageFormatter.java의 기존 코드를 이용, 메일임시삭제 기능을 위한 메시지 테이블 구성

--- maven_webmail/src/main/java/cse/maven_webmail/model/MessageFormatter.java
+++ maven_webmail/src/main/java/cse/maven_webmail/model/MessageFormatter.java
@@ -20,6 +20,43 @@ public class MessageFormatter {
     public MessageFormatter(String userid) {
         this.userid = userid;
     }
+    
+    public String getTempMessageTable(Message[] messages) {
+        StringBuilder buffer = new StringBuilder();
+
+        // 메시지 제목 보여주기
+        buffer.append("<table>");  // table start
+        buffer.append("<tr> "
+                + " <th> No. </td> "
+                + " <th> 보낸 사람 </td>"
+                + " <th> 제목 </td>     "
+                + " <th> 보낸 날짜 </td>   "
+                + " <th> 삭제 </td>   "
+                + " </tr>");
+
+        for (int i = messages.length - 1; i >= 0; i--) {
+            MessageParser parser = new MessageParser(messages[i], userid);
+            parser.parse(false);  // envelope 정보만 필요
+            // 메시지 헤더 포맷
+            // 추출한 정보를 출력 포맷 사용하여 스트링으로 만들기
+            buffer.append("<tr> "
+                    + " <td id=no>" + (i + 1) + " </td> "
+                    + " <td id=sender>" + parser.getFromAddress() + "</td>"
+                    + " <td id=subject> "
+                    + " <a href=show_message.jsp?msgid=" + (i + 1) + " title=\"메일 보기\"> "
+                    + parser.getSubject() + "</a> </td>"
+                    + " <td id=date>" + parser.getSentDate() + "</td>"
+                    + " <td id=delete>"
+                    + "<a href=ReadMail.do?menu="
+                    + CommandType.DELETE_TEMP_MAIL_COMMAND
+                    + "&msgid=" + (i + 1) + "> 삭제 </a>" + "</td>"
+                    + " </tr>");
+        }
+        buffer.append("</table>");
+
+        return buffer.toString();
+//        return "MessageFormatter 테이블 결과";
+    }
 
     public String getMessageTable(Message[] messages) {
         StringBuilder buffer = new StringBuilder();
