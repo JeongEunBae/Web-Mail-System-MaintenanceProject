a7daab2b3b9376b5faa1574d8a4b538ddbc4e2e1
deu3332 <57334493+deu3332@users.noreply.github.com>
2021-06-02 00:17:19 +0900

    Update UserAccept.java

--- maven_webmail/src/main/java/cse/maven_webmail/control/UserAccept.java
+++ maven_webmail/src/main/java/cse/maven_webmail/control/UserAccept.java
@@ -56,13 +56,22 @@ public class UserAccept extends HttpServlet {
             throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         
-            try (PrintWriter out = response.getWriter()){
+            try{
 
-            String className = "com.mysql.cj.jdbc.Driver";
+            //String className = "com.mysql.cj.jdbc.Driver";
+            //Class.forName(className);
+            //String url = "jdbc:mysql://localhost:3306/webmail?serverTimezone=Asia/Seoul";
+            //String User = "jdbctester";
+            //String Password = "1088";
+
+            
+            
+            final String className = "com.mysql.cj.jdbc.Driver";
             Class.forName(className);
-            String url = "jdbc:mysql://localhost:3306/webmail?serverTimezone=Asia/Seoul";
-            String User = "jdbctester";
-            String Password = "1088";
+            final String url = "jdbc:mysql://113.198.235.241:3192/web-mailsystem?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=Asia/Seoul";
+            final String User = "webmailuser";
+            final String Password = "12345";
+                
             conn = DriverManager.getConnection(url, User, Password); 
             
             String change_id = request.getParameter("change_id");
@@ -80,14 +89,6 @@ public class UserAccept extends HttpServlet {
             String plz_sql ="SELECT id,pw,accept_check FROM user_register WHERE accept_check=1"; 
             rs = stmt.executeQuery(plz_sql);   
             
-
-            //String sql_2 ="SELECT id,pw,accept_check FROM user_register WHERE id=? and accept_check=?";  
-            //<<-- SQL 구문 오류 해결불가.. 이 구문으로 돌리면 완성. 현재는 회원정보 여러개 처리불가
-            //r_pstmt = con.prepareStatement(sql_2);
-            //r_pstmt.setString(1,request.getParameter("change_id"));
-            //r_pstmt.setInt(2,1);
-            //rs_2 = r_pstmt.executeQuery(sql_2);
-            //해결완료.    
             
             while(rs.next()){    
                     String server = "127.0.0.1";
@@ -95,12 +96,13 @@ public class UserAccept extends HttpServlet {
                     
                     if(rs.getString("id")!=null && rs.getString("id").equals(change_id)){ // 여기가 입력받은 id값이랑 SQL커서값 동일한 id인지 확인.
 
-                      try {
+                      try(PrintWriter out = response.getWriter()) {
                             UserAdminAgent agent = new UserAdminAgent(server, port, this.getServletContext().getRealPath("."));
                             String userid = rs.getString("id"); 
                             String password = rs.getString("pw");
                             out.println("userid = " + userid + "<br>");
                             out.println("password = " + password + "<br>");
+                            out.println("회원가입 승인 성공!! 뒤로가기를 눌러주세요.");
                             out.flush();
                             if(agent.addUser(userid, password)) {
                                Popup.append("<script>alert('사용자 인증을 완료했습니다.'); location.href='UserStateChange.jsp;</script>");
@@ -117,14 +119,6 @@ public class UserAccept extends HttpServlet {
                 }
             }
 }
-
-            if (query_state >= 1) {
-                Popup.append("<script>alert('회원가입 신청 승인이 완료되었습니다.'); location.href='UserStateChange.jsp; </script>");
-                out.println(Popup.toString());
-            } else {
-                Popup.append("<script>alert('서버상태를 확인해주세요..');location.href='UserStateChange.jsp; </script>");
-                out.println(Popup.toString());
-            }
                 
         } catch (Exception ex) {
          Log log = LogFactory.getLog(UserAccept.class);
